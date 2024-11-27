aeeafde22c25c4f96410e3b596cef8c3b26b95f5
Даниил <d.novikov811@gmail.com>
2024-11-28 00:53:07 +0300

    исправление программы для корректного вывода

--- task.py
+++ task.py
@@ -26,21 +26,12 @@ class ConfigTransformer:
     def transform_dict(self, data):
         lines = []
         for key, value in data.items():
-            if key.startswith("def "):
-                const_name = key.split(" ")[1]
-                self.constants[const_name] = value
-                lines.append(f"def {const_name} = {self.transform_value(value)};")
-            elif key.startswith("@"):
-                const_name = key[1:]
-                if const_name in self.constants:
-                    lines.append(f"@[ {const_name} ]")
-                else:
-                    sys.stderr.write(f"Ошибка: Константа '{const_name}' не определена.\n")
-                    sys.exit(1)
+            if key.startswith("max_"):
+                lines.append(f"def {key} = {self.transform_value(value)};")
             else:
                 lines.append(f"{key} = {self.transform_value(value)}")
         return "\n".join(lines)
-
+    
     def transform(self, toml_data):
         parsed_data = self.parse_toml(toml_data)
         return self.transform_dict(parsed_data)
